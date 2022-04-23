2a31414c8d799c02ff7e1d626861f2c1a8a10423
Thanet_Notebook <thanet_y@yahoo.com>
2022-04-23 20:30:14 +0700

    Add Ep3 print function

---
+++ Ep3.py
@@ -0,0 +1,18 @@
+"""
+learn about Print function
+1. print string
+2. print number,float
+3. print boolean
+
+"""
+print(-1)
+print(0)
+print(1)
+print(3.99)
+print(True)
+print(False)
+print("--------")
+print(2>3)
+print(3>2)
+print("hello \n" + "World")
+print("hello \t" + "World")
