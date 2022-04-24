9cbc8a549a8f50f53505e0a125e6292234737009
Thanet_Notebook <thanet_y@yahoo.com>
2022-04-24 16:00:49 +0700

    add Ep.4

---
+++ Ep4.py
@@ -0,0 +1,15 @@
+# data type and variable
+# variable name
+
+from tkinter import TRUE
+
+
+x = 20 # x is int
+print("ผลลัพท์ is " + str(x)) # convert x to string
+y = 2.35
+z = True
+
+print(x +y)
+print(type(x+y))
+
+
